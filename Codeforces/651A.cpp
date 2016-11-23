e2b51ad58b5ba983d4e2bcaa8bba68598cf4a57f
devanshu-ghosh <devanshu0987@gmail.com>
2016-11-23 10:38:00 +0530

    651A - iterative

--- Codeforces/651A.cpp
+++ Codeforces/651A.cpp
@@ -10,17 +10,22 @@ ll maxi(int a,int b)
     return b;
 }
 ll f(ll A,ll B){
-    if(A==0 || B==0)
-        return 0;
-    if(A==1 && B!=1)
-        return 1+f(A+1,B-2);
-    if(A!=1 && B==1)
-        return 1+f(A-2,B+1);
     if(A==1 && B==1)
         return 0;
-    if(A>=B)
-        return 1+f(A-2,B+1);
-    return 1+f(A+1,B-2);
+    int c=0;
+    while(A>0 && B>0)
+    {
+        if(A>=B){
+            A=A-2;
+            B++;
+        }
+        else{
+            A++;
+            B=B-2;
+        }
+        c++;
+    }
+    return c;
 }
 int main()
 {
